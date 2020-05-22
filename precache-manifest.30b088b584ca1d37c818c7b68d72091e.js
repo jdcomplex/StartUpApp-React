43c7205e665fbd01a28af2a1292d544efe4f4787
Kamal Khanal <kkhanal@braindigit.com>
2020-05-22 11:27:23 +0545

    Updates

---
+++ precache-manifest.30b088b584ca1d37c818c7b68d72091e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78aca34d23a675c083a2f703ced4656",
+    "url": "/StartUpApp-React/index.html"
+  },
+  {
+    "revision": "5a4774cf1a5eafa94ac0",
+    "url": "/StartUpApp-React/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "906ac797aca9549e1272",
+    "url": "/StartUpApp-React/static/css/main.aa6c083a.chunk.css"
+  },
+  {
+    "revision": "5a4774cf1a5eafa94ac0",
+    "url": "/StartUpApp-React/static/js/2.aaa83aa8.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/StartUpApp-React/static/js/2.aaa83aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "906ac797aca9549e1272",
+    "url": "/StartUpApp-React/static/js/main.e6788fa3.chunk.js"
+  },
+  {
+    "revision": "a895c48b282c42b09a36",
+    "url": "/StartUpApp-React/static/js/runtime-main.b19bac1d.js"
+  }
+]);
